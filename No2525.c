56f3e84ded98bd4d5de7b69bfd6959afb4fbcefa
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2023-07-31 18:56:16 +0900

    Create No2525.c

---
+++ No2525.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int h, m;
+   int waitm;
+
+   scanf("%d %d", &h, &m);
+   scanf("%d", &waitm);
+
+   m += waitm;
+
+   if(m >= 60)
+   {
+      h += m / 60;
+      m %= 60;
+   }
+
+   if(h >= 24)
+   {
+      h %= 24;
+   }
+
+   printf("%d %d", h, m);
+   
+   return 0;
+}
