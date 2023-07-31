4e5ac516622d4664210d723a76861242b4eb93f2
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2023-07-31 10:44:08 +0900

    Create BeakJoonNo10869.c

---
+++ BeakJoonNo10869.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    printf("%d\n", a + b);
+    printf("%d\n", a - b);
+    printf("%d\n", a * b);
+    printf("%d\n", a / b);
+    printf("%d\n", a % b);
+
+    return 0;
+}
