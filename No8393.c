4164bd42011f9400ac377845d1a76efb540fae51
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2023-07-31 21:42:13 +0900

    Create No8393.c

---
+++ No8393.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+  int a;
+  int sum = 0;
+
+  scanf("%d", &a);
+
+  for(int i = 0; i <= a; i ++)
+  {
+   sum += i;
+  }
+
+  printf("%d", sum);
+
+  return 0;
+}
