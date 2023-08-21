46d507835d30a3d8f6275fa943bd45304a4fde36
Resistance-R <125963196+Resistance-R@users.noreply.github.com>
2023-08-21 13:55:41 +0900

    Create No5622.c

---
+++ No5622.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main(void)
+{
+  char str[16] = {0, };
+  int time = 0;
+
+  scanf("%s", str);
+
+  for(int i = 0; str[i] != 0; i++)
+  {
+    if(str[i] == 'A' || str[i] == 'B' || str[i] == 'C')
+    {
+      time += 3;
+    }
+
+    else if(str[i] == 'D' || str[i] == 'E' || str[i] == 'F')
+    {
+      time += 4;
+    }
+
+    else if(str[i] == 'G' || str[i] == 'H' || str[i] == 'I')
+    {
+      time += 5;
+    }
+
+    else if(str[i] == 'J' || str[i] == 'K' || str[i] == 'L')
+    {
+      time += 6;
+    }
+
+    else if(str[i] == 'M' || str[i] == 'N' || str[i] == 'O')
+    {
+      time += 7;
+    }
+
+    else if(str[i] == 'P' || str[i] == 'Q' || str[i] == 'R' || str[i] == 'S')
+    {
+      time += 8;
+    }
+
+    else if(str[i] == 'T' || str[i] == 'U' || str[i] == 'V')
+    {
+      time += 9;
+    }
+
+    else if(str[i] == 'W' || str[i] == 'X' || str[i] == 'Y' ||str[i] == 'Z')
+    {
+      time += 10;
+    }
+  }
+
+  printf("%d", time);
+
+  return 0;
+}
